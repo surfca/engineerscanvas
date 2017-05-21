28dd34413b162da0ea25da5f2f03a93cc8771489
Redwood Surf <redwoodsurfcoast@gmail.com>
2017-05-21 09:58:12 -0700

    Added Articles, References and Devise things

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
 class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
 
   def index
   end
 
   def show
   end
-  
+
+  def destroy
+  end
 end
---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,19 @@
+class TagsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
