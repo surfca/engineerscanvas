8534aa1fb86db655ab02510904777e44e56dc50f
Redwood Surf <redwoodsurfcoast@gmail.com>
2017-09-16 17:42:12 -0700

    Added attachment validations

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,4 +1,5 @@
 class Figure < ActiveRecord::Base
   has_attached_file :avatar, styles: { large: "2560x440#", medium: "300x300!", small: "150x150!", thumb: "50x50!" }, default_url: "images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :avatar, presence: true,
+                        content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
