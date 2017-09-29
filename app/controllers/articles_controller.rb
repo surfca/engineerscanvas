df81ae970ee18f32ae3a92949038ed8f81174241
Redwood Surf <redwoodsurfcoast@gmail.com>
2017-09-28 18:31:58 -0700

    Added articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,9 +50,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     if @article.visible
       @figures = @article.figures.order(:order_num)
-      render 'show'
+      render show
     else
-      redirect_to 'index'
+      redirect_to index
     end
   end
 
