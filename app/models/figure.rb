c6e7c83698e6d96471b110dbabaf5f7c513d19f6
Redwood Surf <redwoodsurfcoast@gmail.com>
2017-09-28 17:54:57 -0700

    Added article images

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,5 +1,5 @@
 class Figure < ActiveRecord::Base
-  has_attached_file :avatar, styles: { large: "2200x500#", medium: "1900x400#", small: "150x150!"}, default_url: "images/:style/missing.png"
+  has_attached_file :avatar, styles: { large: "2200x500#", medium: "1000x400#", small: "150x150!"}, default_url: "images/:style/missing.png"
   validates_attachment :avatar, presence: true,
                         content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
